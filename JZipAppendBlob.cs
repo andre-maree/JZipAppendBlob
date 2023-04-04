e5ecb4807cd6828ff18f934da93dc94fa7be485c
Andre <maree.andre@gmail.com>
2023-04-04 20:06:34 +0200

    Add project files.

---
+++ JZipAppendBlob.cs
@@ -0,0 +1,61 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+using System.IO.Compression;
+using System.Text;
+
+namespace JZipAppendBlob
+{
+    public static class JZipAppendBlob
+    {
+        /// <summary>
+        /// Compress JSON data as a string of List<object[]> and append the blob
+        /// </summary>
+        /// <param name="appendBlobClient"></param>
+        /// <param name="data"></param>
+        /// <param name="encodingCode"></param>
+        /// <returns></returns>
+        private static async Task AppendBlobAsync(AppendBlobClient appendBlobClient, string data, int encodingCode = 65001)
+        {
+            byte[] bytes = Encoding.GetEncoding(encodingCode).GetBytes(data);
+
+            using Stream stream = await appendBlobClient.OpenWriteAsync(false);
+
+            using var gzipStream = new GZipStream(stream, CompressionLevel.Optimal);
+
+            await gzipStream.WriteAsync(bytes);
+        }
+
+        /// <summary>
+        /// Decompress JSON data as a string of List<object[]> from an append blob
+        /// </summary>
+        /// <param name="blocClient">Azure.Storage.Blobs.BlobClient</param>
+        /// <param name="isSealed">Use true to get data from an open append blob.
+        /// Use false to get data from a closed or sealed append blob, else use null and a check will be done</param>
+        /// <param name="encodingCode">Any valid ecoding code, 65001 = utf8; 1200 = Unicode; 12000 = utf32</param>
+        /// <returns></returns>
+        private static async Task<string> DownloadBlobAsync(BlobClient blocClient, bool? isSealed = null, int encodingCode = 65001)
+        {
+            using var memoryStream = new MemoryStream();
+
+            using (var decompressStream = new GZipStream(await blocClient.OpenReadAsync(new BlobOpenReadOptions(false)), CompressionMode.Decompress))
+            {
+                await decompressStream.CopyToAsync(memoryStream);
+            }
+
+            byte[] arr = memoryStream.ToArray();
+
+            if (!isSealed.HasValue)
+            {
+                throw new NotImplementedException();
+            }
+
+            if (!isSealed.Value)
+            {
+                arr[^1] = (byte)']';
+            }
+
+            return Encoding.GetEncoding(encodingCode).GetString(arr);
+        }
+    }
+}
